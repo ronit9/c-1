f48545366753cdec24a7ddb27b204e9ca4beb191
ronit9 <darjironit1@gmail.com>
2024-07-23 16:27:57 +0530

    23-07-24

--- PR-2/q1.c
+++ PR-2/q1.c
@@ -8,23 +8,18 @@ main(){
 	scanf("%d",&score);
 	
 	grade = (score >=91 && score <=100) 
-						?  'A' 
-						: (score >=81 && score <=90) 
-							?  'B'
-							:	(score >=71 && score <=80) 
-									?  'C'
-									:	(score >=61 && score <=70) 
-															?  'D'
-															:	(score >=51 && score <=60) 
-																			?	 'E'
-																			:	'f';
-													
-		
-	
-
-		
-	
-	(grade=='H')
+		?  'A' 
+		: (score >=81 && score <=90) 
+			?  'B'
+			:	(score >=71 && score <=80) 
+				?  'C'
+				:	(score >=61 && score <=70) 
+					?  'D'
+					:	(score >=51 && score <=60) 
+						?	 'E'
+						:	'f';
+					
+					(grade=='H')
 					? printf("You are Fail....")
 					: printf("You are Pass\nGRADE = %c",grade);
 	
