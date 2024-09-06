e01f2462d23d298c09b33666fafd6f7d2f65012b
Ronit Darji <34264289+ronit9@users.noreply.github.com>
2024-09-06 09:52:44 +0530

    Add files via upload

--- final exam/q6.c
+++ final exam/q6.c
@@ -8,23 +8,30 @@ int main()
     char name[20] , role[20];
     FILE *fp;
 
-    fp = fopen("data.txt", "w");
+    
 
     printf("enter number of emp :=");
     scanf("%d", &n);
+
+    fp = fopen("data.txt", "w");
 
     for (int i = 0; i < n; i++)
     {
         if (fp != '\0')
         {
+            printf("\n\n");
             printf("\nenter your id :=");
             scanf("%d", &id);
+            fprintf(fp, "ID: %d\n", id);
 
+            fflush(stdin);
             printf("enter your name :=");
-            scanf("%s", &name);
+            scanf(" %[^\n]", name);
+            fprintf(fp, "Name: %s\n", name);
 
             printf("enter your role :=");
-            scanf("%s", &role);
+            scanf(" %[^\n]", role); 
+            fprintf(fp, "role: %s\n", role);
         }
         else{
             printf("file not found");
